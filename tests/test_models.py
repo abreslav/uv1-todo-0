b90e6084ca0904972ac2d76ac1015b1f8b5dc8d1
CodeSpeak <gen@codespeak.dev>
2025-10-09 08:47:53 +0000

    Run: dad27b9d-821f-4a7a-ae6f-cf5730324c7c, Phase: GenerateAndRunTests, Additional ID: (iteration 1)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,20 @@
 import pytest
 from django.test import TestCase
 from django.utils import timezone
+from django.contrib.auth.models import User
 from django_app.models import Todo
 
 
 class TestTodo(TestCase):
     """Unit tests for Todo model"""
+
+    def setUp(self):
+        """Set up test user for Todo model tests"""
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpass123'
+        )
 
     @pytest.mark.timeout(30)
     def test_todo_str_method(self):
@@ -15,19 +24,19 @@ class TestTodo(TestCase):
         """
         # Test with short content (less than 50 chars)
         short_content = "Buy milk"
-        todo = Todo.objects.create(content=short_content)
+        todo = Todo.objects.create(content=short_content, user=self.user)
         expected = f"{short_content}..."
         self.assertEqual(str(todo), expected)
 
         # Test with long content (more than 50 chars)
         long_content = "This is a very long todo item that has more than fifty characters in it"
-        todo_long = Todo.objects.create(content=long_content)
+        todo_long = Todo.objects.create(content=long_content, user=self.user)
         expected_long = f"{long_content[:50]}..."
         self.assertEqual(str(todo_long), expected_long)
 
         # Test with exactly 50 characters
         exactly_50_content = "12345678901234567890123456789012345678901234567890"  # exactly 50 chars
-        todo_50 = Todo.objects.create(content=exactly_50_content)
+        todo_50 = Todo.objects.create(content=exactly_50_content, user=self.user)
         expected_50 = f"{exactly_50_content[:50]}..."
         self.assertEqual(str(todo_50), expected_50)
 
@@ -38,15 +47,15 @@ class TestTodo(TestCase):
         Original method FQN: Todo.is_done
         """
         # Test todo that is not done (marked_as_done_at is None)
-        todo_not_done = Todo.objects.create(content="Test todo not done")
+        todo_not_done = Todo.objects.create(content="Test todo not done", user=self.user)
         self.assertFalse(todo_not_done.is_done)
 
         # Test todo that is done (marked_as_done_at is set)
-        todo_done = Todo.objects.create(content="Test todo done", marked_as_done_at=timezone.now())
+        todo_done = Todo.objects.create(content="Test todo done", marked_as_done_at=timezone.now(), user=self.user)
         self.assertTrue(todo_done.is_done)
 
         # Test todo that becomes done
-        todo_becomes_done = Todo.objects.create(content="Test todo becomes done")
+        todo_becomes_done = Todo.objects.create(content="Test todo becomes done", user=self.user)
         self.assertFalse(todo_becomes_done.is_done)
 
         todo_becomes_done.marked_as_done_at = timezone.now()
